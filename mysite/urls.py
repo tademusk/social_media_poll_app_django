bf83a027ff15d1c892817968cfa839f27da219c1
Tadele Kelayu <tadelsaloove@gmail.com>
2024-08-11 23:44:44 +0300

    migratin applied for poll app

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from poll import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('poll.urls')),
+    path('test', views.tade )
 ]
